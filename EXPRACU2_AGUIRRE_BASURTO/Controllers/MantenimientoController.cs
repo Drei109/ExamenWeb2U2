4746bcf7af3f4d939ab511f2fd8463eb2f7ce737
drei109 <drei.rar@gmail.com>
2018-05-26 19:51:45 -0500

    Persona controller added

---
+++ EXPRACU2_AGUIRRE_BASURTO/Controllers/MantenimientoController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EXPRACU2_AGUIRRE_BASURTO.Models;
+using EXPRACU2_AGUIRRE_BASURTO.ViewModels;
+using AutoMapper;
+
+namespace EXPRACU2_AGUIRRE_BASURTO.Controllers
+{
+    public class MantenimientoController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public MantenimientoController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Mantenimiento
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListarPersonal()
+        {
+            return View();
+        }
+
+        public ActionResult AgregarPersonal()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AgregarPersonal(AgregarPersonalViewModel model)
+        {
+            //var Persona = Mapper.Map<AgregarPersonalViewModel, Persona>(model);
+            //_context.Personal.Add(Persona);
+            //_context.SaveChanges();
+            return View();
+        }
+
+
+    }
+}
---
+++ EXPRACU2_AGUIRRE_BASURTO/ViewModels/MantenimientoViewModels.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using EXPRACU2_AGUIRRE_BASURTO.Models;
+
+namespace EXPRACU2_AGUIRRE_BASURTO.ViewModels
+{
+    public class AgregarPersonalViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255, ErrorMessage = "Este campo es obligatorio")]
+        public string Nombres { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "Apellido Paterno")]
+        public string ApellidoPaterno { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "Apellido Materno")]
+        public string ApellidoMaterno { get; set; }
+
+        [Column(TypeName = "Date")]
+        [Display(Name = "Fecha  de Nacimiento")]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required]
+        [StringLength(8)]
+        public string DNI { get; set; }
+
+        [StringLength(255)]
+        [Display(Name = "Dirección")]
+        public string Direccion { get; set; }
+
+        [StringLength(255)]
+        [Phone]
+        [Display(Name = "Teléfono")]
+        public string Telefono { get; set; }
+
+        [StringLength(255)]
+        [Display(Name = "Estado Civil")]
+        public string EstadoCivil { get; set; }
+
+        [Display(Name = "Cantidad de Hijos")]
+        public byte CantHijos { get; set; }
+
+        [StringLength(255)]
+        public string Sexo { get; set; }
+
+        [StringLength(255)]
+        public string Estado { get; set; }
+
+        public enum SexoEnum
+        {
+            Masculino,
+            Femenino
+        }
+        //public Turno Turno { get; set; }
+    }
+}
